529c1f9c1dfd3ab26018a702f860c44d05f0d242
yutingwu4 <scorp.only@gmail.com>
2021-06-12 15:10:01 -0700

    pulled it to root directory for nodemon to work

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+// const bodyParser = require("body-parser");
+const app = express();
+const apiRouter = require("./src/api.js");
+
+//converts requests from client into readable language for express
+//if there is a json in body of request, unpack it and set it as body property on request object that express is creating
+app.use(express.json());
+//necessary if using <form> in html; parses data on request body into readable language for express
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", apiRouter);
+
+app.get("/healthcheck", (req, res) => {
+  res.send({ hello: "world" });
+});
+
+//global error handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(400).send("Whoops, global error handler caught ya!");
+});
+
+app.listen(3001, (err) => {
+  if (err) console.log("Whoops, error on port 3001!", err);
+  console.log("surveys-api running on port 3001");
+});
